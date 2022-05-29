d6f078ad9e94b46f494daf31a762acdaac5dc2bb
JulienDelaunay <julien.delaunay034@gmail.com>
2022-05-30 01:06:17 +0200

    Adjusted environments and various effects

--- Assets/Global/Scripts/ShaderControl/PositionShaderProperty.cs
+++ Assets/Global/Scripts/ShaderControl/PositionShaderProperty.cs
@@ -10,7 +10,6 @@ public class PositionShaderProperty : ShaderProperty {
 
 	protected override void ApplyProperty() {
 		Vector3 pos = _position.position;
-		Debug.Log(pos);
 		_sharedPropertyBlock.SetVector(_propertyId, new Vector4(pos.x, pos.y, pos.z, 0f));
 	}
 }
