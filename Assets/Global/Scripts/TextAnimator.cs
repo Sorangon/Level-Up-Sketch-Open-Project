c489e87880016667d0d1f93dc24ef3dc797f9402
JulienDelaunay <julien.delaunay034@gmail.com>
2022-05-29 20:07:12 +0200

    Improved text animation

--- Assets/Global/Scripts/TextAnimator.cs
+++ Assets/Global/Scripts/TextAnimator.cs
@@ -1,11 +1,9 @@
-using System.Text;
 using TMPro;
 using UnityEngine;
 
 [ExecuteAlways]
 public class TextAnimator : MonoBehaviour {
 	[SerializeField, Range(0f, 1f)] private float _drawRatio = 1.0f;
-	[SerializeField, Range(0f, 1f)] private float _negativeFillRatio = 0.0f;
 	[SerializeField, Min(1)] private int _loops = 1;
 	[SerializeField] private string _text = "Hello world";
 
@@ -32,20 +30,6 @@ public class TextAnimator : MonoBehaviour {
 		if (charCount > 0) {
 			string croppedText = _text.Substring(0,charCount);
 			_textMesh.text = croppedText;
-			
-			if (_negativeFillRatio > 0f) {
-				int charactersToFill = Mathf.RoundToInt(_negativeFillRatio * charCount);
-				Color[] meshColors = _textMesh.mesh.colors;
-				for (int i = 0; i < charactersToFill; i++) {
-					for (int j = 0; j < 4; j++) {
-						int index = i * 4 + j; 
-						var col = meshColors[index];
-						col.a = 0f;
-						meshColors[index] = col;
-					}
-				}
-				_textMesh.mesh.SetColors(meshColors);
-			}
 		}else {
 			_textMesh.text = string.Empty;
 		}
